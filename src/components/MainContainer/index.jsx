81e02551808516535dba8d9dd06fc63a8b50e783
gabjcav <69957148+gabjcav@users.noreply.github.com>
2021-05-20 17:15:14 +0200

    made themes-section responsive

--- src/components/MainContainer/index.jsx
+++ src/components/MainContainer/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/main.scss";
 import HeroContainer from "../HeroContainer";
+import iconArrow from "../../img/icon-arrow.svg";
 const MainContainer = () => {
   return (
     <main>
@@ -10,20 +11,38 @@ const MainContainer = () => {
         <div id="themes-container">
           <article>
             <p>Design</p>
+            <button className="theme-btn">
+              <img src={iconArrow} alt="Arrow icon for 'Design'" />
+            </button>
           </article>
           <article>
             <p>Reklame</p>
+            <button className="theme-btn">
+              <img src={iconArrow} alt="Arrow icon for 'Reklame' " />
+            </button>
           </article>
           <article>
             <p>Backend</p>
+            <button className="theme-btn">
+              <img src={iconArrow} alt="Arrow icon for 'Backend'" />
+            </button>
           </article>
           <article>
             <p>Frontend</p>
+            <button className="theme-btn">
+              <img src={iconArrow} alt="Arrow icon for 'Frontend'" />
+            </button>
           </article>
           <article>
             <p>UX</p>
+            <button className="theme-btn">
+              <img src={iconArrow} alt="Arrow icon for 'UX'" />
+            </button>
           </article>
         </div>
+      </section>
+      <section>
+        <h3>Slik fungerer det for deg</h3>
       </section>
     </main>
   );
