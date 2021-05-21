32f415ded342fd29c973a58bf0052ead7514f0f6
gabjcav <69957148+gabjcav@users.noreply.github.com>
2021-05-21 10:07:23 +0200

    subjects now in array & own component

--- src/components/MainContainer/index.jsx
+++ src/components/MainContainer/index.jsx
@@ -1,53 +1,19 @@
 import React from "react";
-import "../../styles/main.scss";
+import "./index.scss";
 import HeroContainer from "../HeroContainer";
-import iconArrow from "../../img/icon-arrow.svg";
+import SubjectsContainer from "../SubjectsContainer";
+
 const MainContainer = () => {
   return (
     <main>
       <HeroContainer />
 
-      {/*THEMES SECTION BELOW */}
-
-      <section id="themes">
-        <h2>Mest etterspurte tema</h2>
-        <div id="themes-container">
-          <article>
-            <p>Design</p>
-            <button className="theme-btn">
-              <img src={iconArrow} alt="Arrow icon for 'Design'" />
-            </button>
-          </article>
-          <article>
-            <p>Reklame</p>
-            <button className="theme-btn">
-              <img src={iconArrow} alt="Arrow icon for 'Reklame' " />
-            </button>
-          </article>
-          <article>
-            <p>Backend</p>
-            <button className="theme-btn">
-              <img src={iconArrow} alt="Arrow icon for 'Backend'" />
-            </button>
-          </article>
-          <article>
-            <p>Frontend</p>
-            <button className="theme-btn">
-              <img src={iconArrow} alt="Arrow icon for 'Frontend'" />
-            </button>
-          </article>
-          <article>
-            <p>UX</p>
-            <button className="theme-btn">
-              <img src={iconArrow} alt="Arrow icon for 'UX'" />
-            </button>
-          </article>
-        </div>
-      </section>
+      <SubjectsContainer />
 
       {/* HOW IT WORKS SECTION BELOW */}
+
       <section id="how-it-works">
-        <h3>Slik fungerer det for deg</h3>
+        <h4>Slik fungerer det for deg</h4>
         <div id="hiw-container">
           <article></article>
           <article></article>
